d68af20f72719f1207ebcf7d3b5182d3989b12a3
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-29 05:08:28 +0700

    Print user

--- src/pages/DashboardUser.jsx
+++ src/pages/DashboardUser.jsx
@@ -3,22 +3,23 @@ import { useNavigate, Outlet } from 'react-router-dom';
 
 export default function DashboardUser() {
     const navigate = useNavigate();
+    const getUser = localStorage.getItem('user');
 
     return (
         <div className="p-8">
-            <h1 className="text-2xl font-bold mb-6">Dashboard User | <small>Dexa Group</small></h1>
+            <h1 className="text-2xl font-bold mb-6">Dashboard User (Dexa Group) | <small>Selamat Datang, { getUser ?? 'user' }</small></h1>
 
             <div className="flex space-x-1 bg-gray-100 p-4 rounded-lg shadow-md mb-6">
-                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition">
+                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition"  onClick={() => navigate('/user')}>
                     Beranda
                 </button>
-                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition">
+                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition"  onClick={() => navigate('/user/profile')}>
                     Profile
                 </button>
-                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition">
+                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition" onClick={() => navigate('/user/absensi')}>
                     Absensi
                 </button>
-                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition">
+                <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition" onClick={() => navigate('/user/summary')}>
                     Summary Absensi
                 </button>
                 <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-100 hover:text-gray-600 transition" onClick={() => navigate('/logout')}>
