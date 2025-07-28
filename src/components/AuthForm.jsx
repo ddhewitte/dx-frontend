f90e176e423b1606e48fe594efc78ab8a69ce7b1
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-07-28 16:31:16 +0700

    Auth Login with redirect
    - check auth login
    - get auth profile

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -11,19 +11,19 @@ export default function AuthForm({ onLogin }){
     };
 
     return(
-        <div class="flex items-center h-screen w-full">
-            <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-            <span class="block w-full text-xl uppercase font-bold mb-4">Login</span>      
-                <form class="mb-4" onSubmit={submitProcess}>
-                <div class="mb-4 md:w-full">
-                    <label for="email" class="block text-xs mb-1">Username</label>
-                    <input class="w-full border rounded p-2 outline-none focus:shadow-outline" type="text" name="user" id="user" placeholder="Username" />
+        <div className="flex items-center h-screen w-full">
+            <div className="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+            <span className="block w-full text-xl uppercase font-bold mb-4">Login</span>      
+                <form className="mb-4" onSubmit={submitProcess}>
+                <div className="mb-4 md:w-full">
+                    <label htmlFor="email" className="block text-xs mb-1">Username</label>
+                    <input className="w-full border rounded p-2 outline-none focus:shadow-outline" type="text" name="user" id="user" placeholder="Username" value={user} onChange={(e) => setUser(e.target.value)} required/>
                 </div>
-                <div class="mb-6 md:w-full">
-                    <label for="password" class="block text-xs mb-1">Password</label>
-                    <input class="w-full border rounded p-2 outline-none focus:shadow-outline" type="password" name="password" id="password" placeholder="Password" />
+                <div className="mb-6 md:w-full">
+                    <label htmlFor="password" className="block text-xs mb-1">Password</label>
+                    <input className="w-full border rounded p-2 outline-none focus:shadow-outline" type="password" name="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                 </div>
-                <button class="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded">Login</button>
+                <button className="bg-green-500 hover:bg-green-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded">Login</button>
                 </form>
                 <span>you can login as HR admin or user</span>
             </div>
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,36 @@
 import AuthForm from "../components/AuthForm"
+import { authLogin, getUserProfile} from "../services/AuthService";
+import { useNavigate } from 'react-router-dom';
 
-export default function Auth(){
-    return (
-        <AuthForm />
-    )
+export default function Auth() {
+    const navigate = useNavigate();
+
+    const loginProcess = async (data) => {
+        try {
+            const res = await authLogin(data); 
+            const token = res.accessToken;
+
+            const resProfile = await getUserProfile(token);
+
+            //Store it to localStorage
+            localStorage.setItem('token', token);
+            localStorage.setItem('role', resProfile.role);
+            localStorage.setItem('userId', resProfile.id);
+            localStorage.setItem('user', resProfile.user);
+
+            console.log('resProfile.role', resProfile.role);
+
+            //redirect after login
+            if (resProfile.role === 'ADMIN') {
+                console.log('admin');
+                navigate('/dashboard-admin');
+            } else {
+                navigate('/dashboard-user');
+            }
+        } catch (err) {
+            alert('Login error: ' + err.message);
+        }
+    };
+
+    return <AuthForm onLogin={loginProcess} />;
 }
